c6e75e450534ea968ae5042dddf08c3410bdfadf
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-19 08:56:57 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -40,6 +40,6 @@ rm /tmp/mobirise-latest.deb
 
 # Cleanup
 # -------
-apt remove snapd
-rm -Rf /var/cache/apt/archives
-rm -Rf /var/lib/snapd
+# apt remove snapd
+# rm -Rf /var/cache/apt/archives
+# rm -Rf /var/lib/snapd
